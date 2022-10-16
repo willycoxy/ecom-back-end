9b2ef9b56c0dae1179991f3dff94afdb50ff99f6
willycoxy <williamjwcox@gmail.com>
2022-10-15 23:52:54 -0400

    inital commit

---
+++ Develop/routes/api/tag-routes.js
@@ -0,0 +1,103 @@
+const router = require('express').Router();
+const { Tag, Product, ProductTag } = require('../../models');
+
+// The `/api/tags` endpoint
+
+router.get('/', (req, res) => {
+  // find all tags
+  Tag.findAll({
+  // be sure to include its associated Product data
+    include: [
+      {
+        model: Product
+      }
+    ]
+  })
+  .then(dbTagData => res.json(dbTagData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
+});
+
+router.get('/:id', (req, res) => {
+  // find a single tag by its `id`
+  Tag.findOne({
+  // be sure to include its associated Product data
+  where: {
+    id: req.params.id
+  },
+  include: [
+    {
+      model: Product,
+      attributes: ['id', 'product_name','price','stock','category_id']
+    }
+  ]
+  })
+  .then(dbTagData => {
+    if (!dbTagData) {
+      res.status(404).json({ message: 'No tag found with this id'});
+      return;
+    }
+    res.json(dbTagData);
+  })
+  .catch(err =>{
+    console.log(err)
+    res.status(500).json(err);
+  });
+});
+
+router.post('/', (req, res) => {
+  Tag.create({
+    tag_name: req.body.tag_name
+  })
+  .then(dbTagData => res.json(dbTagData))
+  .catch(err =>{
+    console.log(err);
+    res.status(500).json(err);
+  })
+});
+
+router.put('/:id', (req, res) => {
+  // update a tag's name by its `id` value
+  Tag.update(req.body, {
+  // individualHooks: true,
+  where: {
+    id: req.params.id
+  }
+  })
+  .then(dbTagData => {
+    if (!dbTagData[0]) {
+      res.status(404).json({ message: 'No tag found with this id' });
+      return;
+    }
+    res.json(dbTagData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  })
+});
+
+router.delete('/:id', (req, res) => {
+  // delete on tag by its `id` value
+  Tag.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbTagData => {
+      if (!dbTagData) {
+        res.status(404).json({ message: 'No Tag found with this id' });
+        return;
+      }
+      res.json(dbTagData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+
+module.exports = router;
---
+++ README.md
@@ -0,0 +1,40 @@
+# ECOM-BACK-END
+
+  ## Table of Contents 
+  * [Demo](#demolink)
+  * [Built With](#built-with)
+  * [Description](#description)
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+  
+  ## DEMOLINK
+  https://drive.google.com/file/d/1mIyQig6VVHF1yvxk5D8bR7W84ws4ih9c/view
+
+  ## Built With 
+  - Javascript
+  - Express.js
+  - MySQL
+  - Sequelize
+
+  ## Description 
+   This is a back end application for a e-commerce website that uses up to date technologies so this company can compete with others. Using MYSQL, Sequelize to achieve this. 
+
+  ## Installation
+    npm install express
+    npm install sequelize
+    npm install mysql2
+
+  ## Usage
+  node server.js to start server. This is used as a backend for a e-coms website.
+
+  ## Contributing 
+  Created by Will Cox 
+
+  ## Questions 
+  If you have any questions regarding this app, please reach be using my GitHub or email below.
+
+  * Github: [willycoxy](https://github.com/willycoxy)
+  * Email: [williamjwcox@gmail.com](mailto:williamjwcox@gmail.com)
